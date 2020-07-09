4189f9f35268d7e979efc754764a1240aa7263c1
Nicole Zambrano <znicole473@gmail.com>
2020-07-09 15:38:52 -0500

    #4 Seeders

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        $password = Hash::make('052914');
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@prueba.com',
+            'password' => $password,
+            ]);
+        for ($i = 0; $i < 15; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+                ]);
+        }
+    }
+}
